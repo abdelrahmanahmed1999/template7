d5107d191e061bdba07f4c7b2a5f4ce8d81b3458
abdelrahmanahmed1999 <abdelrahmanahmedtarek1999@gmail.com>
2020-07-25 20:49:51 +0200

    sample template

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*global $,jquery,alert*/
+
+$(document).ready(function () {
+    "user strict"
+
+    $(window).scroll(function () {
+        var nav = $('.navbar');
+        $(window).scrollTop() >= nav.height() ? nav.addClass('scrolled') :  nav.removeClass('scrolled');
+    });
+
+
+    var select=$('.tab ul li');
+
+    select.click(function () {
+        
+        $(this).addClass('selected').siblings().removeClass('selected');
+
+       if($(this).data('class') == ".tab-one"){
+           $('.tab-one-content').css('display','block').siblings().css('display','none');
+       }
+       else if($(this).data('class') == ".tab-two"){
+        $('.tab-two-content').css('display','block').siblings().css('display','none');
+       }
+       else {
+        $('.tab-three-content').css('display','block').siblings().css('display','none');
+
+       }
+
+    });
+
+
+
+});
